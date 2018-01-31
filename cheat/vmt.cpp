5830178aa009a71f16bebc757c1f92c90eb597b8
swog <sanicsupersonic@gmail.com>
2018-01-31 15:49:26 -0600

    Update vmt.cpp

--- cheat/vmt.cpp
+++ cheat/vmt.cpp
@@ -1,4 +1,25 @@
 
+#include <Windows.h>
 #include "vmt.h"
 
+typedef void* (*CreateInterfaceFn)(const char *pName, int *pReturnCode);
+
+void* VMT::GetVirtual(void* instance, int index)
+{
+}
+
+void* VMT::HookVirtual(void* instance, int index, void* replacement)
+{
+}
+
+void* VMT::GetInterface(char* module, char* name)
+{
+	CreateInterfaceFn CreateInterface = (CreateInterfaceFn)GetProcAddress(GetModuleHandleA(module), "CreateInterface");
+	return CreateInterface(name);
+}
+
+void* VMT::GetInterfaceVersion(char* module, char* prefix)
+{
+}
+	
 VMT vmt;
