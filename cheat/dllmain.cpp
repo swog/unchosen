e3755a0bf6af392f2df407eb996e389ff5b6a805
swog <sanicsupersonic@gmail.com>
2018-01-30 23:26:29 -0600

    Update dllmain.cpp

--- cheat/dllmain.cpp
+++ cheat/dllmain.cpp
@@ -1,8 +1,20 @@
 
 #include <Windows.h>
 
-
+DWORD WINAPI ThreadCharter(LPVOID lpParameter)
+{
+	FreeLibraryAndExitThread((HMODULE)lpParameter, 0);
+	return 0;
+}
 
 BOOL WINAPI DllMain(HINSTANCE hInstDll, DWORD fdwReason, LPVOID lpvReserved)
 {
+	if (fdwReason == DLL_PROCESS_ATTACH)
+	{
+		DisableThreadLibraryCalls(hInstDll);
+		CreateThread(0, 0, ThreadCharter, 0, 0, 0);
+		return TRUE;
+	}
+	
+	return FALSE;
 }
