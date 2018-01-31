838a937fdbeb7d7b3fc60f30b190788c75b0ac82
swog <sanicsupersonic@gmail.com>
2018-01-31 15:18:37 -0600

    Update menu.cpp

--- cheat/menu.cpp
+++ cheat/menu.cpp
@@ -1,10 +1,15 @@
 
 #include "menu.h"
+#include "surface.h"
 #include "../junk.h"
 
 void Menu::GetCursorPos(int& cx, int& cy)
 {
 	JUNK(cursorpos);
+}
+
+void Menu::DrawCheckBox(char* label, int x, int y, int w, int h, bool& setting)
+{
 }
 
 Menu::Menu(void)
