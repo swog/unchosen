c0d1740537a6efb37e3676f64b78c18b9dbecef7
swog <sanicsupersonic@gmail.com>
2018-01-30 23:22:24 -0600

    Create junk.h
    junk definition

---
+++ junk.h
@@ -0,0 +1,17 @@
+
+// Generated using random.org
+#define JUNK(x) \
+	char _##x_0[] = "GndPkl1FW1"; \
+	char _##x_1[] = "FVHKm8ayM3"; \
+	char _##x_2[] = "Q0ZsgpTbHd"; \
+	char _##x_3[] = "14mxUjr9cU"; \
+	char _##x_4[] = "zG9IThb01f"; \
+	char _##x_5[] = "7MFZirwoWZ"; \
+	char _##x_6[] = "zeO4B4Wb3p"; \
+	char _##x_7[] = "lzmcp7VMXS"; \
+	char _##x_8[] = "MGc8tIkZdW"; \
+	char _##x_9[] = "ltGOPdDUDV"; \
+	char _##x_a[] = "OM90wx0wpL"; \
+	char _##x_b[] = "WdQBqpQuj5"; \
+	char _##x_c[] = "CSWhJyWWA3"; \
+	char _##x_d[] = "yqTnPVrj3D"
