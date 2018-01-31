6879a7a63842439185c8240104b766943b97b013
swog <sanicsupersonic@gmail.com>
2018-01-31 15:18:14 -0600

    Update menu.h

--- cheat/menu.h
+++ cheat/menu.h
@@ -8,7 +8,7 @@ private:
 	Vector m_Pos;
 	Vector m_Size;
 	char* m_Title;
-	const Vector m_CheckBoxSize = Vecotr(0, 0, 0);
+	const Vector m_CheckBoxSize = Vector(0, 0, 0);
 	
 	void GetCursorPos(int& cx, int& cy);
 	void DrawCheckbox(char* label, int x, int y, int w, int h, bool& setting);
