dfb1caef9bb854348968d7e02bbbab11303ce3b9
swog <sanicsupersonic@gmail.com>
2018-01-31 15:41:59 -0600

    Update vector.h

--- cheat/vector.h
+++ cheat/vector.h
@@ -8,7 +8,7 @@ public:
 
 	Vector(float _x, float _y, float _z) { Init(_x, _y, _z); }
 	Vector(float _xyz) { Init(_xyz); }
-	Vector(void) { Init(0.f, 0.f, 0.f); }
+	Vector(void) { Zero(); }
 	
 	void Init(float _x, float _y, float _z) { x = _x; y = _y; z = _z; }
 	void Init(float _xyz = 0.f) { x = y = z = _xyz; }
@@ -16,6 +16,8 @@ public:
 	float* Base(void) { return (float*)this; }
 	float Length(void) { return (float)sqrt((double)(x * x + y * y + z * z));
 	
+	void Zero(void) { x = y = z = 0.f; }
+			    
 	bool operator==(const Vector& other) { return x == other.x && y == other.y && z == other.z; }
 	bool operator!=(const Vector& other) { return x != other.x || y != other.y || z != other.z; }
 	Vector& operator-(void) { return Vector(-x, -y, -z); }
@@ -24,5 +26,5 @@ public:
 	Vector& operator*(const Vector& other) { return Vector(x * other.x, y * other.y, z * other.z); }
 	Vector& operator/(float f) { return Vector(x / f, y / f, z / f); }
 	Vector& operator/(const Vector& other) { return Vector(x / other.x, y / other.y, z / other.z); }
-	float& operator[](int i) { return ((float*)this)[i];
+	float& operator[](int i) { return ((float*)this)[i]; }
 };
