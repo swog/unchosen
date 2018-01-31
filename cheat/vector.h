6c39e0fbda2ed0a4e296f6eb1cb787d0d65065e9
swog <sanicsupersonic@gmail.com>
2018-01-31 15:33:17 -0600

    Update vector.h

--- cheat/vector.h
+++ cheat/vector.h
@@ -15,6 +15,5 @@ public:
 	
 	bool operator==(const Vector& other) { return x == other.x && y == other.y && z == other.z; }
 	bool operator!=(const Vector& other) { return x != other.x || y != other.y || z != other.z; }
-	bool operator>(const Vector& other) { return x > other.x && y > other.y && z > other.z; }
 	float& operator[](int i) { return ((float*)this)[i];
 };
