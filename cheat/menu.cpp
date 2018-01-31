2798c689ace6a26fc9d3c62af1fe38ff3cd7f164
swog <sanicsupersonic@gmail.com>
2018-01-30 23:43:27 -0600

    Create menu.cpp

---
+++ cheat/menu.cpp
@@ -0,0 +1,18 @@
+
+#include "menu.h"
+#include "../junk.h"
+
+void Menu::GetCursorPos(int& cx, int& cy)
+{
+	JUNK(cursorpos);
+}
+
+Menu::Menu(void)
+{
+	JUNK(menu);
+}
+
+void Menu::PaintTraverse(void)
+{
+	JUNK(paint);
+}
