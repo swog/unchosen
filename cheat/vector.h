a89d49d97ff805f275c6c0c6e283ebd384ca0713
swog <sanicsupersonic@gmail.com>
2018-01-31 15:29:08 -0600

    Create vector.h

---
+++ cheat/vector.h
@@ -0,0 +1,13 @@
+
+class Vector
+{
+public:
+	float x, y, z;
+
+	Vector(float _x, float _y, float _z) { Init(_x, _y, _z); }
+	Vector(float _xyz) { Init(_xyz); }
+	Vector(void) { Init(0.f, 0.f, 0.f); }
+	
+	void Init(float _x, float _y, float _z) { x = _x; y = _y; z = _z; }
+	void Init(float _xyz = 0.f) { x = y = z = _xyz; }
+};
