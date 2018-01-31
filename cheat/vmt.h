8a17864a444843fc6f6da7873ec1f89a7893a767
swog <sanicsupersonic@gmail.com>
2018-01-31 15:45:59 -0600

    Update vmt.h

--- cheat/vmt.h
+++ cheat/vmt.h
@@ -4,6 +4,8 @@ class VMT
 public:
 	void* GetVirtual(void* instance, int index);
 	void* HookVirtual(void* instance, int index, void* replacement);
+	void* GetInterface(char* module, char* name);
+	void* GetInterfaceVersion(char* module, char* prefix);
 };
 
 extern VMT vmt;
