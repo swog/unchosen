07b7f6a185d28c22112e5544ae174156bb704a1a
swog <sanicsupersonic@gmail.com>
2018-01-31 20:33:00 -0600

    Create painttraverse.cpp

---
+++ cheat/painttraverse.cpp
@@ -0,0 +1,26 @@
+
+#include "painttraverse.h"
+#include "vmt.h"
+#include "../junk.h"
+#include <cstring>
+
+PaintTraverseFn oPaintTraverse;
+
+void __fastcall hkPaintTraverse(void* self, void*, VPANEL vguiPanel, bool forceRepaint, bool allowForce)
+{
+	oPaintTraverse(self, vguiPanel, forceRepaint, allowForce);
+	
+	static VPANEL savedPanel = 0;
+	if (!savedPanel)
+	{
+		if (!strcmp(panel->GetClassname(vguiPanel), "MatSystemTopPanel"))
+		{
+			savedPanel = vguiPanel;
+		}
+	}
+	
+	if (savedPanel != vguiPanel)
+	{
+		return;
+	}
+}
