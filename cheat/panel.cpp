6d37feb317c7219d9d3a6c9bfa3814331bd51f58
swog <sanicsupersonic@gmail.com>
2018-01-31 20:23:27 -0600

    Create panel.cpp

---
+++ cheat/panel.cpp
@@ -0,0 +1,12 @@
+
+#include "panel.h"
+#include "vmt.h"
+#include "../junk.h"
+
+const char* Panel::GetClassname(VPANEL vguiPanel)
+{
+	typedef const char*(__thiscall* Fn)(void*, VPANEL);
+	return ((Fn)vmt.GetVirtual(this, 36))(this, vguiPanel);
+}
+
+Panel* panel = 0;
