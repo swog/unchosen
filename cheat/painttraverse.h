165839a66f8144da2045a8bb4d4ff767147f0686
swog <sanicsupersonic@gmail.com>
2018-01-31 22:29:56 -0600

    Update painttraverse.h

--- cheat/painttraverse.h
+++ cheat/painttraverse.h
@@ -1,7 +1,12 @@
 
+#ifndef PAINTTRAVERSE_H
+#define PAINTTRAVERSE_H
+
 #include "panel.h"
 
 typedef void(__thiscall* PaintTraverseFn)(void*, VPANEL, bool, bool);
 extern PaintTraverseFn oPaintTraverse;
 
 void __fastcall hkPaintTraverse(void* self, void*, VPANEL vguiPanel, bool forceRepaint, bool allowForce = true);
+
+#endif
