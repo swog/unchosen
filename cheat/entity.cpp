b687013b5340ebbadecb0f246ab4bc4935292897
swog <sanicsupersonic@gmail.com>
2018-02-01 19:06:53 -0600

    sync with private (JUNK!, classname)

--- cheat/entity.cpp
+++ cheat/entity.cpp
@@ -1,13 +1,24 @@
 
 #include "entity.h"
 #include "engine.h"
+#include "vmt.h"
+#include "../junk.h"
 
 int Entity::entindex(void)
 {
+	JUNK(index);
 	return *(int*)(this + 0x64);
 }
 
 bool Entity::GetPlayerInfo(player_info_t* pinfo)
 {
+	JUNK(info);
 	return engine->GetPlayerInfo(entindex(), pinfo);
+}
+
+const char* Entity::GetClassname(void)
+{
+	JUNK(classname);
+	typedef const char*(__thiscall* Fn)(void*);
+	return ((Fn)vmt.GetVirtual(this, 139))(this);
 }
