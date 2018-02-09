2cdf47a8a45e5b1cafc2e55404fe1d136c6524f2
swog <sanicsupersonic@gmail.com>
2018-02-08 22:27:38 -0600

    Update offsets.cpp

--- cheat/offsets.cpp
+++ cheat/offsets.cpp
@@ -9,6 +9,7 @@ Offsets::Offsets()
 void Offsets::Init()
 {
 	m_fFlags = netvars.FindOffsets("DT_BasePlayer", "m_fFlags");
+	m_vecViewOffset = netvars.FindOffset("DT_BasePlayer", "m_vecViewOffset[0]");
 }
 
 Offsets offsets;
