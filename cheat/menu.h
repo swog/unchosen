60e38750d1e8c8a8dc4d1b8c8ecb670b2f7f593a
swog <sanicsupersonic@gmail.com>
2018-02-08 22:58:16 -0600

    Update menu.h

--- cheat/menu.h
+++ cheat/menu.h
@@ -8,23 +8,8 @@
 
 class Menu
 {
-private:
-	Vector m_Pos;
-	Vector m_Size;
-	char* m_Title;
-	
-	const Vector m_CheckBoxSize = Vector(30, 30, 0);
-	const Color m_CheckBoxColor = Color(255, 255, 255, 255);
-	
-	void GetCursorPos(int& cx, int& cy);
-	bool IsCursorOver(int x, int y, int w, int h);
-	void DrawRect(int x0, int y0, int x1, int y1, Color col);
-	
-	void DrawCheckbox(char* label, int x, int y, int w, int h, bool& setting);
-	
 public:
-	Menu(void);
-	void PaintTraverse(void);
+	void PaintTraverse();
 };
 
 extern Menu menu;
