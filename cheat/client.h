7ea8360a08c064500600e96f6891f822df623919
Bjordsvennson <reid820000@gmail.com>
2018-02-08 19:24:16 -0800

    Fixed structures

--- cheat/client.h
+++ cheat/client.h
@@ -2,61 +2,87 @@
 #ifndef CLIENT_H
 #define CLIENT_H
 
+class CRecvProxyData;
 class RecvTable;
 class RecvProp;
 
-enum SendPropType
+typedef enum
 {
 	DPT_Int = 0,
 	DPT_Float,
 	DPT_Vector,
-	DPT_VectorXY, // Only encodes the XY of a vector, ignores Z
+	DPT_VectorXY,
 	DPT_String,
-	DPT_Array,	// An array of the base types (can't be of datatables).
+	DPT_Array,
 	DPT_DataTable,
 	DPT_NUMSendPropTypes
+} SendPropType;
+
+class DVariant
+{
+public:
+	union
+	{
+		float       m_Float;
+		int         m_Int;
+		const char* m_pString;
+		void*       m_pData;
+		float       m_Vector[3];
+	};
+
+	SendPropType    m_Type;
+};
+
+class CRecvProxyData
+{
+public:
+	const RecvProp* m_pRecvProp;
+	DVariant        m_Value;
+	int             m_iElement;
+	int             m_ObjectID;
 };
 
 class RecvProp
 {
 public:
-	const char* m_pVarName;
-	SendPropType m_RecvType;
-	int m_Flags;
-	int m_StringBufferSize;
-	bool m_bInsideArray;
-	const void* m_pExtraData;
-	RecvProp* m_pArrayProp;
-	void* m_DataTableProxyFn;
-	RecvTable* m_pDataTable;
-	int m_Offset;
-	int m_ElementStride;
-	int m_nElements;
-	const char* m_pParentArrayPropName;
+	const char*             m_pVarName;
+	SendPropType            m_RecvType;
+	int                     m_Flags;
+	int                     m_StringBufferSize;
+	bool                    m_bInsideArray;
+	const void*             m_pExtraData;
+	RecvProp*               m_pArrayProp;
+	void*					m_ArrayLengthProxy;
+	void*					m_ProxyFn;
+	void*					m_DataTableProxyFn;
+	RecvTable*              m_pDataTable;
+	int                     m_Offset;
+	int                     m_ElementStride;
+	int                     m_nElements;
+	const char*             m_pParentArrayPropName;
 };
 
 class RecvTable
 {
 public:
-	RecvProp* m_pProps;
-	int m_nProps;
-	void* m_pDecoder;
+	RecvProp*   m_pProps;
+	int         m_nProps;
+	void*       m_pDecoder;
 	const char* m_pNetTableName;
-	bool m_bInitialized;
-	bool m_bInMainList;
+	bool        m_bInitialized;
+	bool        m_bInMainList;
 };
 
 class ClientClass
 {
 public:
-	void* m_pCreateFn;
-	void* m_pCreateEventFn;
-	const char* m_pNetworkName;
-	RecvTable* m_pRecvTable;
-	ClientClass* m_pNext;
-	int m_ClassID;
+	void*           m_pCreateFn;
+	void*           m_pCreateEventFn;
+	const char*     m_pNetworkName;
+	RecvTable*      m_pRecvTable;
+	ClientClass*    m_pNext;
+	int             m_ClassID;
 };
-
 class Client
 {
 public:
