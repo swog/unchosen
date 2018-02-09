169fa7c8cfe265285022ef1e5fec2cb80c7152a9
Bjordsvennson <reid820000@gmail.com>
2018-02-08 19:25:18 -0800

    fixed

--- cheat/netvars.h
+++ cheat/netvars.h
@@ -12,12 +12,14 @@ class NetVars
 {
 	ClientClass* m_pClasses;
 	std::vector<RecvTable*> m_pTables;
-public:
-	void Init(void);
-
+	int m_ExtraOffset;
+	
 	int RecurseForOffset(RecvTable* pTable, char* childname);
 	RecvProp* RecurseForProp(RecvTable* pTable, char* childname);
-
+public:
+	
+	void Init(void);
+	
 	int FindOffset(char* tablename, char* childname);
 	RecvProp* FindProp(char* tablename, char* childname);
 };
